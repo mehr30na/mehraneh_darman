b499a9676cc336a3dc9665d846ee5f800c3f0a7b
Alireza Mehrsina <mehr30na@gmail.com>
2017-01-21 10:40:09 +0330

    seed

--- server/database/factories/ModelFactory.php
+++ server/database/factories/ModelFactory.php
@@ -21,3 +21,43 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Patient::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'national_code' => $faker->numberBetween(4000000,5000000),
+        'idcrd_number' => $faker->numberBetween(4000000,5000000),
+        'birt_hdate' => $faker->date($format = 'Y-m-d', $max = '-20 years'),
+        'father_name' => $faker->name,
+        'birth_place' => $faker->city,
+        'living_place' => $faker->city,
+        'file_number' => $faker->numberBetween(4000,5000),
+        'gender' => $faker->randomElement($array = array ('m','f')),
+        'phone' => $faker->phoneNumber,
+        'mobile' => $faker->phoneNumber,
+        'cancer_type' => $faker->numberBetween(1,5),
+        'marital_status' => $faker->randomElement($array = array ('s','m')),
+        'doctor_name' => $faker->name,
+        'picture' => $faker->imageUrl(500, 400 , 'cats', true, 'Faker'),
+    ];
+});
+
+$factory->define(App\Expense::class, function (Faker\Generator $faker) {
+    return [
+        'letter_number' => $faker->name,
+        'national_code' => $faker->numberBetween(4000000,5000000),
+        'idcrd_number' => $faker->numberBetween(4000000,5000000),
+        'birt_hdate' => $faker->date($format = 'Y-m-d', $max = '-20 years'),
+        'father_name' => $faker->name,
+        'birth_place' => $faker->city,
+        'living_place' => $faker->city,
+        'file_number' => $faker->numberBetween(4000,5000),
+        'gender' => $faker->randomElement($array = array ('m','f')),
+        'phone' => $faker->phoneNumber,
+        'mobile' => $faker->phoneNumber,
+        'cancer_type' => $faker->numberBetween(1,5),
+        'marital_status' => $faker->randomElement($array = array ('s','m')),
+        'doctor_name' => $faker->name,
+        'picture' => $faker->imageUrl(500, 400 , 'cats', true, 'Faker'),
+    ];
+});
