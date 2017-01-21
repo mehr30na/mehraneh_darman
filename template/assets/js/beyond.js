fdcc305befd50b7f5900a548950f132e2ffd7183
Alireza Mehrsina <mehr30na@gmail.com>
2017-01-21 10:00:10 +0330

    Template Files

---
+++ template/assets/js/beyond.js
@@ -0,0 +1,57 @@
+/*Sets Themed Colors Based on Themes*/
+
+
+
+//Gets Theme Colors From Selected Skin To Use For Drawing Charts
+
+
+//Handle RTL SUpport for Changer CheckBox
+
+
+//Checks Not to Do rtl-support for Arabic and Persian Demo Pages
+
+
+/*Loading*/
+
+
+/*Account Area --> Setting Button*/
+
+
+/*Toggle FullScreen*/
+
+
+
+
+
+/*Handles ToolTips*/
+
+
+    
+
+
+
+
+
+
+
+/*#region handle Settings*/
+
+
+
+/*#endregion handle Settings*/
+
+
+
+/*#region Get Colors*/
+//Get colors from a string base on theme colors
+
+/*#endregion Get Colors*/
+
+
+//Switch Classes Function
+
+
+
+//Add Classes Function
+
+
